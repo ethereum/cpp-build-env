f3bd154e1f023a11d973b457208049ec4bcb3b0f
Paweł Bylica <chfast@gmail.com>
2019-10-23 14:48:56 +0200

    Install and expose additional LLVM tools for clang toolchain

--- install_default_compiler.sh
+++ install_default_compiler.sh
@@ -4,12 +4,12 @@ version=$2
 
 case "$1" in
     gcc)
-        package=g++
+        packages="g++-$version"
         cc=gcc
         cxx=g++
         ;;
     clang)
-        package=clang
+        packages="clang-$version llvm-$version"
         cc=clang
         cxx=clang++
         ;;
@@ -19,13 +19,19 @@ case "$1" in
         ;;
 esac
 
-package=$package-$version
-
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -qq update
-sudo apt-get install -yq --no-install-recommends $package
+sudo apt-get install -yq --no-install-recommends $packages
 sudo rm -rf /var/lib/apt/lists/*
+
 sudo ln -s /usr/bin/$cc-$version /usr/bin/$cc
 sudo ln -s /usr/bin/$cc-$version /usr/bin/cc
 sudo ln -s /usr/bin/$cxx-$version /usr/bin/$cxx
 sudo ln -s /usr/bin/$cxx-$version /usr/bin/cpp
+
+# Expose all LLVM tools without the version suffix.
+if [ "$1" = clang ]; then
+  cd /usr/bin
+  sudo ln -s ../lib/llvm-$version/bin/llc .
+  sudo ln -s ../lib/llvm-$version/bin/llvm-* .
+fi
